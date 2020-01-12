ea498448ebac8038396379e260934f9392141003
Kevin <keviinnalvarez2015@gmail.com>
2020-01-12 13:18:37 -0400

    agg dependecia para realizar peticiones

--- src/components/create_user.js
+++ src/components/create_user.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
+
+  async componentDidMount () {
+    const res = await axios.get('http://localhost:3000/api/users');
+    console.log(res.data);
+  }
+
   render () {
     return (
       <div>CreateUser</div>
